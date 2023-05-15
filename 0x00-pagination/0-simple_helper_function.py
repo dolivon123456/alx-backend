bccabfb0a00aa4011978a5b19be802dd98ae3007
dolivon123456 <dolivon555@gmail.com>
2023-05-15 18:46:33 +0000

    index range

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,9 +4,8 @@ This module defines the function index_range
 """
 
 def index_range(page: int, page_size: int) -> tuple:
-    """
-    return a tuple of size two containing a start and end index
-    """
+    """ return a tuple of size two containing a start and end index """
+
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
