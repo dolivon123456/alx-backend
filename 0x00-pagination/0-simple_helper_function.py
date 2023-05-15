e5ad6cc160dc21de7d9fd4dfe9f95e14621abf99
dolivon123456 <dolivon555@gmail.com>
2023-05-15 18:49:26 +0000

    index range

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""
-This module defines the function index_range
+""" Write a function named index_range that takes
+two integer arguments page and page_size.
 """
 
+
 def index_range(page: int, page_size: int) -> tuple:
-    """ return a tuple of size two containing a start and end index """
+    """ create a tuple containing page and the page_size """
 
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
